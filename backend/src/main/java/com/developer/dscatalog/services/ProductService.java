a4057ef25bee1a7d598dbb695fb14b1e71571f7b
Notebook <kaique.andruid@gmail.com>
2024-05-23 14:28:06 -0300

    Product insert, update, delete

--- backend/src/main/java/com/developer/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/developer/dscatalog/services/ProductService.java
@@ -1,7 +1,10 @@
 package com.developer.dscatalog.services;
 
+import com.developer.dscatalog.dto.CategoryDTO;
 import com.developer.dscatalog.dto.ProductDTO;
+import com.developer.dscatalog.entities.Category;
 import com.developer.dscatalog.entities.Product;
+import com.developer.dscatalog.repositories.CategoryRepository;
 import com.developer.dscatalog.repositories.ProductRepository;
 import com.developer.dscatalog.services.exceptions.DatabaseException;
 import com.developer.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -22,6 +25,9 @@ public class ProductService {
     @Autowired
     private ProductRepository  repository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @Transactional(readOnly = true)
     public Page<ProductDTO> findAllPaged (PageRequest pageRequest) {
         Page<Product> list = repository.findAll(pageRequest);
@@ -38,7 +44,7 @@ public class ProductService {
     @Transactional
     public ProductDTO insert(ProductDTO dto) {
         Product entity = new Product();
-       // entity.setName(dto.getName());
+        copyDtoToEntity(dto, entity);
         entity = repository.save(entity);
         return new ProductDTO(entity);
     }
@@ -47,7 +53,7 @@ public class ProductService {
     public ProductDTO update(Long id, ProductDTO dto) {
         try {
             Product entity = repository.getReferenceById(id);
-            //entity.setName(dto.getName());
+            copyDtoToEntity(dto, entity);
             entity = repository.save(entity);
             return new ProductDTO(entity);
         }
@@ -66,6 +72,20 @@ public class ProductService {
         }
         catch (DataIntegrityViolationException e) {
             throw new DatabaseException("Falha de integridade referencial");
+        }
+    }
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
+        entity.setName(dto.getName());
+        entity.setDate(dto.getDate());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity.getCategories().clear();
+        for (CategoryDTO catDto : dto.getCategories()) {
+            Category category = categoryRepository.getReferenceById(catDto.getId());
+            entity.getCategories().add(category);
         }
     }
 }
