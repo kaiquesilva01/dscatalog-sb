12c974298e6ea917f6e4eb7c41ed9ef9d55f4ebe
Notebook <kaique.andruid@gmail.com>
2024-06-04 12:47:30 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/developer/dscatalog/entities/Role.java
+++ backend/src/main/java/com/developer/dscatalog/entities/Role.java
@@ -1,7 +1,13 @@
 package com.developer.dscatalog.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_role")
 public class Role {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authority;
 
