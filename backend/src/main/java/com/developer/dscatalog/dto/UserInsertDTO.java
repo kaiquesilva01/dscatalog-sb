85094c8459d76954255d46f2bc49125ba15a64fb
Notebook <kaique.andruid@gmail.com>
2024-06-07 09:48:36 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/developer/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/developer/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.developer.dscatalog.dto;
 
+import com.developer.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO{
 
     private String password;
